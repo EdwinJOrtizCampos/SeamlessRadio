163c888cb196004f34c63fa3c3da1eb628aa6ca2
ORCA <edortcam@gmail.com>
2026-02-22 16:39:45 +0000

    feat: Add main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/view/resources')
+
+from PySide6.QtWidgets import QApplication
+from view.widgets.MainWindow import MainWindow
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
